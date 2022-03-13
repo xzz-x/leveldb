15d7a3448b2b45cfe32702471e02f73fbbf46f4f
xzz-x <1461135947@qq.com>
2022-03-13 19:28:47 +0800

    内存管理的注释

--- util/arena.cc
+++ util/arena.cc
@@ -10,21 +10,22 @@ static const int kBlockSize = 4096;
 
 Arena::Arena()
     : alloc_ptr_(nullptr), alloc_bytes_remaining_(0), memory_usage_(0) {}
-
+// Area隶属于memtable;当memtable结束生命周期时；Area的生命周期随之结束;Area所属的内存池也将释放
 Arena::~Arena() {
   for (size_t i = 0; i < blocks_.size(); i++) {
     delete[] blocks_[i];
   }
 }
-
+// 当前的请求分配内存需要分配一个新的内存块
 char* Arena::AllocateFallback(size_t bytes) {
+  // 如果需要分配的空间大于块大小的四分之一;新分配的指针独占一个内存块;大小为bytes
   if (bytes > kBlockSize / 4) {
     // Object is more than a quarter of our block size.  Allocate it separately
     // to avoid wasting too much space in leftover bytes.
     char* result = AllocateNewBlock(bytes);
     return result;
   }
-
+  // 不足块大小的四分之一时;与其待分配的内存共用一个内存块
   // We waste the remaining space in the current block.
   alloc_ptr_ = AllocateNewBlock(kBlockSize);
   alloc_bytes_remaining_ = kBlockSize;
@@ -34,20 +35,26 @@ char* Arena::AllocateFallback(size_t bytes) {
   alloc_bytes_remaining_ -= bytes;
   return result;
 }
-
+// 以对齐的方法分配内存；目的是为了加快cpu访问的速度
 char* Arena::AllocateAligned(size_t bytes) {
+  // 判断计算机的位数
   const int align = (sizeof(void*) > 8) ? sizeof(void*) : 8;
   static_assert((align & (align - 1)) == 0,
                 "Pointer size should be a power of 2");
+  // 取模运算
   size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align - 1);
+  // 计算对齐时需要补齐的内存大小
   size_t slop = (current_mod == 0 ? 0 : align - current_mod);
   size_t needed = bytes + slop;
   char* result;
+  // 分配内存操作
   if (needed <= alloc_bytes_remaining_) {
+    // 补齐的数据在前;实际分配的数据在后
     result = alloc_ptr_ + slop;
     alloc_ptr_ += needed;
     alloc_bytes_remaining_ -= needed;
   } else {
+    // 当前内存块剩余内存不足时；可以直接分配一个新的内存块；而不需要进行补齐操作
     // AllocateFallback always returned aligned memory
     result = AllocateFallback(bytes);
   }
@@ -55,6 +62,7 @@ char* Arena::AllocateAligned(size_t bytes) {
   return result;
 }
 
+// 分配block_bytes字节的内存块出来
 char* Arena::AllocateNewBlock(size_t block_bytes) {
   char* result = new char[block_bytes];
   blocks_.push_back(result);
--- util/arena.h
+++ util/arena.h
@@ -13,6 +13,7 @@
 
 namespace leveldb {
 
+// Arena用于LevelDb中的内存管理
 class Arena {
  public:
   Arena();
@@ -39,16 +40,20 @@ class Arena {
   char* AllocateNewBlock(size_t block_bytes);
 
   // Allocation state
+  // 当前分配的内存块的指针
   char* alloc_ptr_;
+  // 剩余字节数
   size_t alloc_bytes_remaining_;
 
   // Array of new[] allocated memory blocks
+  // 内存池;每个指针指向一个已经分配的内存块；在Arena的析构函数中将会释放掉所有分配的内存块
   std::vector<char*> blocks_;
 
   // Total memory usage of the arena.
   //
   // TODO(costan): This member is accessed via atomics, but the others are
   //               accessed without any locking. Is this OK?
+  // 记录Arena中使用的内存大小
   std::atomic<size_t> memory_usage_;
 };
 
@@ -57,12 +62,14 @@ inline char* Arena::Allocate(size_t bytes) {
   // 0-byte allocations, so we disallow them here (we don't need
   // them for our internal use).
   assert(bytes > 0);
+  // 首先判断当前内存块是否满足需求
   if (bytes <= alloc_bytes_remaining_) {
     char* result = alloc_ptr_;
     alloc_ptr_ += bytes;
     alloc_bytes_remaining_ -= bytes;
     return result;
   }
+  // 不满足则需要;则需要使用新的内存块来满足内存的分配
   return AllocateFallback(bytes);
 }
 
